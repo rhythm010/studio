5c5c44918c4c642af47723f91bdd263069fc88d9
Rhythm Khanna <rhythm.khanna8@gmail.com>
2025-04-17 16:08:20 +0000

    checkpoint-col reverse

--- src/app/pages/Landing/LandingPage.tsx
+++ src/app/pages/Landing/LandingPage.tsx
@@ -4,11 +4,11 @@ import SelectionSection from './SelectionSection';
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col pt-3 pb-4">
+    <div className="bg-gray-900 min-h-screen flex flex-col pt-3 pb-4 ">
       <div id="display_container" className="flex-1">
         <DisplaySection />
       </div>
-      <div id="selection_container" className="flex-1">
+      <div id="selection_container" className="flex-1 flex flex-col-reverse">
         <SelectionSection />
       </div>
     </div>
