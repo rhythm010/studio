b7d9d7dabf98ca4b30f65bc9f4400daebfc264a8
rhythm010 <rhythm.khanna8@gmail.com>
2025-04-19 12:01:48 +0400

    feat: update in padding

--- src/app/pages/Landing/LandingPage.tsx
+++ src/app/pages/Landing/LandingPage.tsx
@@ -4,7 +4,7 @@ import SelectionSection from './SelectionSection';
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col pt-3 pb-4 ">
+    <div className="bg-gray-900 min-h-screen flex flex-col pt-[4rem] pb-4 ">
       <div id="display_container" className="flex-1">
         <DisplaySection />
       </div>
