e2c6d5ec989bee8297a029524484550e385a9b5f
Rhythm Khanna <rhythm.khanna8@gmail.com>
2025-04-19 07:58:51 +0000

    Header: icons, props, modal connect added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,41 @@
-function Header() {
-  return (
-    <div className="bg-gray-800 p-4 shadow-lg w-full">
-        <h1 className="text-white">PagePilot</h1>
-    </div>
+'use client';
+import { useModal } from '@/components/ui/Modal';
+import { Button } from '@/components/ui/button';
+import { Icons } from '@/components/icons';
 
+interface HeaderProps {
+  showBack?: boolean;
+  showReset?: boolean;
+
+}
+
+function Header({ showBack = true, showReset = true }: HeaderProps) {
+  const { openModal } = useModal();
+  const onReset = () => {
+      openModal('Reset Modal', 'md')
+  }
+
+  return (
+    <div className="bg-gray-800 p-2 shadow-lg w-full flex items-center justify-between">
+        {showBack && (
+          <div >
+            <Button className='bg-gray-800 hover:bg-gray-700' variant="default" size="sm">
+                <Icons.backArrow className="mr-2 h-4 w-4" />
+              </Button>
+          </div>
+        )}
+        <div>
+          <h1 className="text-white">PagePilot</h1>
+        </div>
+        {showReset && ( 
+            
+          <div>
+            <Button className='bg-gray-800 hover:bg-gray-700' variant="default" size="sm" onClick={onReset}>
+              <Icons.reset className="mr-2 h-4 w-4" />
+            </Button>
+          </div>
+        )}
+    </div>
   );
 }
 
