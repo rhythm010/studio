8636c9f32c0c705b3f4a60fe29f148c909f29145
Rhythm Khanna <rhythm.khanna8@gmail.com>
2025-04-19 11:56:45 +0000

    changes before ai changes

--- src/app/pages/Landing/DisplaySection.tsx
+++ src/app/pages/Landing/DisplaySection.tsx
@@ -1,17 +1,17 @@
 const DisplaySection: React.FC = () => {
   return (
     <div className="flex h-full pt-[2rem]">
-      <div className="flex-1 flex items-center justify-center border-white">
+      <div id="companion_1" className="flex-1 flex items-center justify-center border-white">
         <div className="image-wrapper">
           <img className="max-w-[11rem]" src="/displaySection/sideGuard.png" alt="Display" />
         </div>
       </div>
-      <div className="flex-1 flex items-center justify-center border-white">
-        {/* <div className="image-wrapper p-7">
+      <div id="center_stage" className="flex-1 flex items-center justify-center border-white">
+        <div className="image-wrapper p-7">
           <img src="/displaySection/male.png" alt="Display" />
-        </div> */}
+        </div>
       </div>
-      <div className="flex-1 flex items-center justify-center border-white">
+      <div id="companion_2" className="flex-1 flex items-center justify-center border-white">
         <div className="image-wrapper">
         <img className="max-w-[11rem]" src="/displaySection/sideGuard.png" alt="Display" />
         </div>
