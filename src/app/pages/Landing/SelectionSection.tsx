5dafeb7c2803f2913f27f82119171d4212c346c6
Rhythm Khanna <rhythm.khanna8@gmail.com>
2025-04-19 11:11:47 +0000

    submit button added

--- src/app/pages/Landing/SelectionSection.tsx
+++ src/app/pages/Landing/SelectionSection.tsx
@@ -11,10 +11,16 @@ interface SelectionSectionProps {
 
 const SelectionSection: React.FC<SelectionSectionProps> = () => {
   const [activeOption, setActiveOption] = useState<string | null>(null);
-  const { t } = useTranslation('common');
+  const { t } = useTranslation(['common']);
 
-  return (
+  return (<>
+  <div className="w-full rounded-xl pr-3 pl-3">
+      <button className="w-full bg-white text-center border-gray-200 p-2 px-3 rounded-[0.5rem]">
+        {t('submit', { ns: 'common' })}
+      </button>
+    </div>
     <div id="selection_container" className="rounded-xl w-full p-3">
+
       <div id="gender_radio" className="bg-white flex justify-between items-center w-full border-r-4 rounded-t-[10px] p-4">
         <div className='w-1/2'>
           <label className="inline-flex items-center w-full justify-center ">
@@ -23,44 +29,49 @@ const SelectionSection: React.FC<SelectionSectionProps> = () => {
           </label>
         </div>
         <div className="bg-gray-300 w-[1px] h-8">
-          </div>
+        </div>
         <div className='w-1/2'>
           <label className="inline-flex items-center w-full justify-center">
-          <input type="radio" className="form-radio" name="options" value="option2" />
+            <input type="radio" className="form-radio" name="options" value="option2" />
 
             <span id="gender_female" className="ml-2">{t('female')}</span>
           </label>
         </div>
-        </div>
-        
-        <div className='bg-white justify-normal flex-col w-full items-center rounded-b-[10px]'>
-          {/* section - 1 */}
-          <div
-            id="option_1"
-            className={`flex justify-between items-start border border-b-gray-200 pt-4 pb-4 pr-2 pl-3 ${activeOption === 'option_1' ? 'bg-gray-100' : ''}`}
-            onClick={() => setActiveOption('option_1')}
-          >
-            <div>
-              <div id="selection_first_tier" className="text-xl font-bold">{t('first_tier_title')}</div>
-              <div id="first_tier_description"  className="font-thin text-xs">{t('first_tier_description')}</div>
-            </div>
-            <div id="price_first_tier" className="font-bold">{t('first_tier_price')}د.إ</div>
-          </div> 
-
-          {/* section - 2 */}
-          <div
-            id="option_2"
-            className={`flex justify-between items-start pt-4 pb-4 pr-2 pl-3 ${activeOption === 'option_2' ? 'bg-gray-100 rounded-b-[10px]' : ''}`}
-            onClick={() => setActiveOption('option_2')}
-          >
-            <div>
-              <div id="selection_second_tier"  className="text-xl font-bold">{t('second_tier_title')}</div>
-              <div id="second_tier_description"  className="font-light text-xs">{t('second_tier_description')}</div>
-            </div>
-            <div id="price_second_tier" className="font-bold">{t('second_tier_price')}د.إ</div>
-          </div>
-        </div>
       </div>
+
+      <div id="tier_options_container" className='bg-white justify-normal flex-col w-full items-center rounded-b-[10px]'>
+        {/* section - 1 */}
+        <div
+          id="option_1"
+          className={`flex justify-between items-start border border-b-gray-200 pt-4 pb-4 pr-2 pl-3 ${activeOption === 'option_1' ? 'bg-gray-100' : ''}`}
+          onClick={() => setActiveOption('option_1')}
+        >
+          <div>
+            <div id="selection_first_tier" className="text-xl font-bold">{t('first_tier_title')}</div>
+            <div id="first_tier_description" className="font-thin text-xs">{t('first_tier_description')}</div>
+          </div>
+          <div id="price_first_tier" className="font-bold">{t('first_tier_price')}د.إ</div>
+        </div>
+
+        {/* section - 2 */}
+        <div
+          id="option_2"
+          className={`flex justify-between items-start pt-4 pb-4 pr-2 pl-3 ${activeOption === 'option_2' ? 'bg-gray-100 rounded-b-[10px]' : ''}`}
+          onClick={() => setActiveOption('option_2')}
+        >
+          <div>
+            <div id="selection_second_tier" className="text-xl font-bold">{t('second_tier_title')}</div>
+            <div id="second_tier_description" className="font-light text-xs">{t('second_tier_description')}</div>
+          </div>
+          <div id="price_second_tier" className="font-bold">{t('second_tier_price', { ns: 'common' })}د.إ</div>
+        </div>
+
+      </div>
+    </div>
+    
+  </>
+
+
   );
 };
 
