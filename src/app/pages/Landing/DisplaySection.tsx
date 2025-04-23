a237ad0ff620f30103a0d6fad16f8dd9e87db94d
Rhythm Khanna <rhythm.khanna8@gmail.com>
2025-04-23 18:30:02 +0000

    client img position correct

--- src/app/pages/Landing/DisplaySection.tsx
+++ src/app/pages/Landing/DisplaySection.tsx
@@ -39,27 +39,25 @@ const DisplaySection: React.FC<DisplaySectionProps> = ({ selectedGender }) => {
   const isCompanion1Hidden = currentGender === 'male';
 
   return (
-    <div className="flex h-full pt-[2rem]">
-      <div id="companion_1" className={`flex-1 flex items-center justify-center border-white`}>
-        <div className="image-wrapper">
-          <img className="max-w-[11rem]" src="/displaySection/sideGuard_white_shirt.png" alt="Companion 1" />
+    <div className="relative h-full pt-[2rem]">
+          <div id="companion_1" className={`flex items-center justify-center absolute left-0 top-[10rem] h-full`}>
+              <div className="image-wrapper">
+                  <img className="max-w-[11rem]" src="/displaySection/sideGuard_white_shirt.png" alt="Companion 1" />
+              </div>
+          </div>
+          <div id="center_stage" className="border-white w-full h-full">
+              <div className="image-wrapper p-7">
+                  <img id="client_img" 
+                  className={`absolute w-[11rem] left-1/2 top-[12rem] -translate-x-1/2 transition-opacity duration-500 ${isFadingOut ? 'opacity-0' : (shouldBeVisible ? 'opacity-100' : 'opacity-0')}`} src={`/displaySection/${currentGender}.png`} alt="Display" />
+              </div>
+          </div>
+          <div id="companion_2" className="flex items-center justify-center absolute right-0 top-[10rem] h-full">
+              <div className="image-wrapper">
+                  <img className="max-w-[11rem]" src="/displaySection/sideGuard_white_shirt.png" alt="Companion 2" />
+              </div>
         </div>
       </div>
-      <div id="center_stage" className="border-white">
-        <div className="image-wrapper p-7">
 
-          <img id="client_img" 
-          className={`absolute w-[11rem] left-[7.5rem] top-[12rem] transition-opacity duration-500 ${isFadingOut ? 'opacity-0' : (shouldBeVisible ? 'opacity-100' : 'opacity-0')}`} 
-          src={`/displaySection/${currentGender}.png`} alt="Display" />
-
-        </div>
-      </div>
-      <div id="companion_2" className="flex-1 flex items-center justify-center border-white">
-        <div className="image-wrapper">
-        <img className="max-w-[11rem]" src="/displaySection/sideGuard_white_shirt.png" alt="Companion 2" />
-        </div>
-      </div>
-    </div>
   );
 };
 
