072e8469eb7dc22b610f41f723796956b683edbe
Rhythm Khanna <rhythm.khanna8@gmail.com>
2025-05-11 10:45:44 +0000

    header dev mode button added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Button} from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { Switch } from '@/components/ui/switch';
@@ -17,8 +17,9 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ showBack = true }) => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [isDevMode, setIsDevMode] = useState<boolean>(false);
   const { closeModal } = useModal();
-  const { i18n, t } = useTranslation();
+  const { i18n, t } = useTranslation('common');
 
   const store = useCompanionStore();
   const router = useRouter();
@@ -35,7 +36,7 @@ const Header: React.FC<HeaderProps> = ({ showBack = true }) => {
   };
 
   const handleReset = () => {
- router.push('/login')
+    router.push('/login')
   };
 
   const handleGetStore = () => {
@@ -43,10 +44,10 @@ const Header: React.FC<HeaderProps> = ({ showBack = true }) => {
   };
 
   return (
-    <div className="bg-gray-800 shadow-lg h-16 w-full flex items-center justify-between">
+    <header className="bg-gray-800 shadow-lg h-16 w-full flex items-center justify-between">
       {showBack && (
         <div>
-          <Button onClick={handleModalOpen}  className="bg-gray-800 hover:bg-gray-700" variant="default" size="sm">
+          <Button onClick={handleModalOpen} className="bg-gray-800 hover:bg-gray-700" variant="default" size="sm">
             <Icons.backArrow className="mr-2 h-4 w-4" />
           </Button>
         </div>
@@ -58,36 +59,45 @@ const Header: React.FC<HeaderProps> = ({ showBack = true }) => {
         <Tooltip open={isMenuOpen}>
           <TooltipTrigger asChild>
             <Button className="bg-gray-800 hover:bg-gray-700" variant="default" size="sm" onClick={() => {
-              setIsMenuOpen(!isMenuOpen);}}>
+              setIsMenuOpen(!isMenuOpen);
+            }}>
               <Icons.menu className="mr-2 h-4 w-4" />
             </Button>
           </TooltipTrigger>
           <TooltipContent className="flex flex-col items-start">
             <div className='flex flex-row items-center space-x-2'>
-              <Switch id="airplane-mode" onCheckedChange={handleToggleChange}/>
-              <label htmlFor="airplane-mode">en-ar</label>
+              <Switch id="dev-mode" checked={isDevMode} onCheckedChange={setIsDevMode} />
+              <label htmlFor="dev-mode">dev-mode</label>
             </div>
-            <Button onClick={handleReset} className='mt-2' size='sm' variant='ghost'>reset</Button>
-            <Button onClick={handleGetStore} className='mt-2' size='sm' variant='ghost'>getStore</Button>
-            <Button onClick={() => router.push('/login')} className='mt-2' size='sm' variant='ghost'>
-              login
-            </Button>
-            <Button onClick={() => router.push('/landing')} className='mt-2' size='sm' variant='ghost'>
-              landing
-            </Button>
-            <Button onClick={() => router.push('/in-service')} className='mt-2' size='sm' variant='ghost'>
-              in-service
-            </Button>
-            <Button onClick={() => router.push('/end-service')} className='mt-2' size='sm' variant='ghost'>
-              End Service
-            </Button>
-            <Button onClick={() => router.push('/introduction')} className='mt-2' size='sm' variant='ghost'>
-              Introduction
-            </Button>
+            {isDevMode && (
+              <>
+                <div className='flex flex-row items-center space-x-2 mt-2'>
+                  <Switch id="airplane-mode" onCheckedChange={handleToggleChange} />
+                  <label htmlFor="airplane-mode">en-ar</label>
+                </div>
+                <Button onClick={handleReset} className='mt-2' size='sm' variant='ghost'>reset</Button>
+                <Button onClick={handleGetStore} className='mt-2' size='sm' variant='ghost'>getStore</Button>
+                <Button onClick={() => router.push('/login')} className='mt-2' size='sm' variant='ghost'>
+                  /login
+                </Button>
+                <Button onClick={() => router.push('/landing')} className='mt-2' size='sm' variant='ghost'>
+                  /landing
+                </Button>
+                <Button onClick={() => router.push('/in-service')} className='mt-2' size='sm' variant='ghost'>
+                  /in-service
+                </Button>
+                <Button onClick={() => router.push('/end-service')} className='mt-2' size='sm' variant='ghost'>
+                  /End Service
+                </Button>
+                <Button onClick={() => router.push('/introduction')} className='mt-2' size='sm' variant='ghost'>
+                  /Introduction
+                </Button>
+              </>
+            )}
           </TooltipContent>
         </Tooltip>
       </div>
-    </div>
+    </header>
   );
 };
 export default Header;
